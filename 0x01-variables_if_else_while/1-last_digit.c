27cdc4fea57c497740d7f7b3a5cfe683a0f286f5
Your name <courageoduche76@gmail.com>
2023-02-17 18:01:43 -0800

    fix

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,17 +23,17 @@ int main(void)
 	
 	if (m > 5)
 	{
-		printf("and is greater than 5");
+		printf(" and is greater than 5");
 	}
 	
 	if (m == 0)
 	{
-		printf("and is 0");
+		printf(" and is 0");
 	}
 
         if (m < 6 && m != 0)
 	{
-		printf("%d is zero\n", n);
+		printf(" and is less than 6 and not 0");
 	}
 
 	return (0);
