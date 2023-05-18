302d5e58528807938ad17be6d09bc4e9aa8e25e7
Korage-code <courageoduche76@gmail.com>
2023-05-18 10:52:20 -0700

    All done

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,17 @@
 
 int main(void)
 {
-	printf("Size of char: %lu byte(s)\n", sizeof(char));
-	printf("Size of int: %lu byte(s)\n", sizeof(int));
-	printf("Size of long: %lu byte(s)\n", sizeof(long));
-	printf("Size of long long: %lu byte(s)\n", sizeof(long) * 2);
-	printf("Size of float: %lu byte(s)\n", sizeof(float));
+	int a;
+	long int b;
+	long int c;
+	char d;
+	float f;
+
+	printf("Size of a char: %lu byte(s)\n", sizeof(d));
+	printf("Size of an int: %lu byte(s)\n", sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(c) * 2);
+	printf("Size of a float: %lu byte(s)\n", sizeof(f));
 
 	return (0);
 }
